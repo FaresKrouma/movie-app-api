542ecb389afc95afd16f9c797e698085fc85aafe
Fares Krouma <fareskrouma@hotmail.com>
2022-05-17 08:00:13 +0200

    installed Cors

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 const app = express(); // instance of expresss => application
+app.use(cors());
 const port = 4000;
-
+const users = [
+   { name: "Mary", password: "test123" },
+   { name: "John", password: "Ilovekittens" },
+];
 app.get("/", (req, res) => {
    res.send("Hello World!");
 });
+app.get("/users", (req, res) => {
+   res.json(users);
+});
+
+//TODO 1. instal bodyparser to accept post body
+//TODO 2. create a POST endpoint with /login
+//Todo 3. Send email + pw from the userform in the FE to this newly freated backend endpoint
+//TODO 5. check passwords
 
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`);
